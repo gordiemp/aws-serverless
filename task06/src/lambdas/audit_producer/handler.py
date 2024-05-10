e7e6d4f7cc55af0fde54edb6cfd56bc9b08d617e
Maksym Hordienko <mhordiienko@gmail.com>
2024-05-10 13:04:46 +0300

    Task06

--- task06/src/lambdas/audit_producer/handler.py
+++ task06/src/lambdas/audit_producer/handler.py
@@ -1,13 +1,14 @@
-import datetime
-import os
-import uuid
-
 import boto3
+import datetime
+import uuid
 
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
 
 _LOG = get_logger('AuditProducer-handler')
+dynamodb = boto3.resource("dynamodb")
+audit_table = dynamodb.Table("cmtr-1a3f035e-Audit-test")
+
 
 class AuditProducer(AbstractLambda):
 
@@ -16,56 +17,35 @@ class AuditProducer(AbstractLambda):
         
     def handle_request(self, event, context):
 
-        _LOG.info("Incoming event: %s", event)
+        _LOG.info(f"Event: {event}")
+        
+        for record in event["Records"]:
 
-        dynamodb = boto3.resource('dynamodb')
-        conf_table_name = os.environ.get['CONFIGURATION_TABLE']
-        audit_table_name = os.environ.get['AUDIT_TABLE']
-        _LOG.info(f'CONFIGURATION_TABLE: {conf_table_name}')
-        _LOG.info(f'AUDIT_TABLE: {audit_table_name}')
-        table = dynamodb.Table(audit_table_name)
-
-        now = datetime.datetime.now()
-        iso_format = now.isoformat()
-
-        for record in event.get('Records', []):
-            _LOG.info("Processing record: %s", record)
-            
-            if record['eventName'] == 'INSERT':
-                item = {
-                    "id": str(uuid.uuid4()),
-                    "itemKey": record['dynamodb']['Keys']['key']['S'],
-                    "modificationTime": iso_format,
+            if record["eventName"] == "INSERT":
+                audit_item = {
                     "newValue": {
-                        "key": record['dynamodb']['NewImage']['key']['S'],
-                        "value": int(record['dynamodb']['NewImage']['value']['N'])
+                        "key": record["dynamodb"]["NewImage"]["key"]["S"],
+                        "value": int(record["dynamodb"]["NewImage"]["value"]["N"])
                     }
                 }
-            elif record['eventName'] == 'MODIFY':
-                item = {
-                    "id": str(uuid.uuid4()),
-                    "itemKey": record['dynamodb']['Keys']['key']['S'],
-                    "modificationTime": iso_format,
+            elif record["eventName"] == "MODIFY":
+                audit_item = {
                     "updatedAttribute": "value",
-                    "oldValue": int(record['dynamodb']['OldImage']['value']['N']),
-                    "newValue": int(record['dynamodb']['NewImage']['value']['N'])
+                    "oldValue": int(record["dynamodb"]["OldImage"]["value"]["N"]),
+                    "newValue": int(record["dynamodb"]["NewImage"]["value"]["N"])
                 }
             else:
-                _LOG.warn("Unexpected event type: %s", record['eventName'])
                 continue
             
-            try:
-                response = table.put_item(Item=item)
-                _LOG.info(f'DynamoDb response: {response}')
-                _LOG.info(f'Added item to Audit table: {item}')
-            except Exception as error:
-                _LOG.error(f"Failed to put item to table. Exception: {error}")
-
-        return {
-            'message': 'Audit record handled',
-        }
+            audit_item["id"] = str(uuid.uuid4())
+            audit_item["itemKey"] = record["dynamodb"]["NewImage"]["key"]["S"]
+            audit_item["modificationTime"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+            _LOG.info(f"Audit item: {audit_item}")
+            audit_table.put_item(Item=audit_item)
+    
 
 HANDLER = AuditProducer()
+
 
 def lambda_handler(event, context):
     return HANDLER.lambda_handler(event=event, context=context)
--- task06/tests/test_audit_producer/test_success.py
+++ task06/tests/test_audit_producer/test_success.py
@@ -1,6 +1,5 @@
 from tests.test_audit_producer import AuditProducerLambdaTestCase
 
-
 class TestSuccess(AuditProducerLambdaTestCase):
 
     def test_success(self):
